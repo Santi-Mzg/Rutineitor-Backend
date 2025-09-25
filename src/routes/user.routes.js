b588174b1c33bae468412631650ffa9d54879fc2
Santi <santimaszong@gmail.com>
2025-09-25 13:44:55 -0300

    Authentication fixed 1.0

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { authRequire } from '../middlewares/validateToken.js'
-import { deleteUser, getUsers, getUser, getUserMilestones } from '../controllers/user.controller.js';
+import { deleteUser, getUsers, getUser } from '../controllers/user.controller.js';
 
 
 const router = Router();
@@ -8,7 +8,6 @@ const router = Router();
 router.get('/users/', authRequire, getUsers);
 router.get('/user/:username', authRequire, getUser);
 router.delete('/user/:id', authRequire, deleteUser);
-router.get('/user/milestones', authRequire, getUserMilestones);
 
 
 export default router;
